0a13cc0fa888806c9f89b6be7b141408d88efab8
Ian ODonnell <ianodonnellk@gmail.com>
2024-09-30 14:28:27 -0400

    Anonymous buttons

--- vue/src/views/AnonymousView.vue
+++ vue/src/views/AnonymousView.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <h1>Here's what you would see if you were not logged in.</h1>
   </div>
+  <div>
+    <h2>
+      <router-link v-bind:to="{ name: 'login' }">Login.</router-link>
+    </h2>
+    <h2>
+      <router-link v-bind:to="{ name: 'register' }">Create Account.</router-link>
+    </h2>
+  </div>
 </template>
 
 <script>
