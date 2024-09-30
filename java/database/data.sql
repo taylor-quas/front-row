16992019f35ff437f020a62481705ed0fa7df711
Seth Neal <sethaneal1@gmail.com>
2024-09-30 12:15:01 -0400

    first database version

--- java/database/data.sql
+++ java/database/data.sql
@@ -3,4 +3,6 @@ BEGIN TRANSACTION;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+INSERT INTO genres (genre_name) VALUES ('blues'), ('classical'), ('country'), ('electronic'), ('folk'), ('hip-hop'), ('jazz'), ('latin'), ('new age'), ('pop'), ('r&b'), ('reggae'), ('rock');
+
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,14 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS emails;
+DROP TABLE IF EXISTS bands;
+DROP TABLE IF EXISTS user_band;
+DROP TABLE IF EXISTS images;
+DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS band_genre;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS events;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -14,25 +22,17 @@ CREATE TABLE emails (
     email_id SERIAL,
     email varchar(50) NOT NULL UNIQUE,
     email_user int NOT NULL,
-    CONSTRAINT FK_users FOREIGN KEY (email_user) REFERENCES users(user_id)
-)
-
-CREATE TABLE user_band (
-    user_id int NOT NULL,
-    band_id int NOT NULL,
-    CONSTRAINT FK_users FOREIGN KEY (user_id) REFERENCES users(user_id),
-    CONSTRAINT FK_bands FOREIGN KEY (band_id) REFERENCES bands(band_id)
-)
+    CONSTRAINT FK_email_user FOREIGN KEY (email_user) REFERENCES users(user_id)
+);
 
 CREATE TABLE bands (
     band_id SERIAL,
     band_name varchar(50) NOT NULL UNIQUE,
     band_description varchar(200) NOT NULL,
     band_manager_id int NOT NULL,
-    band_hero_image_id int NOT NULL,
+    band_hero_image varchar(200) NOT NULL,
     CONSTRAINT PK_band PRIMARY KEY (band_id),
-    CONSTRAINT FK_users FOREIGN KEY (band_manager_id) REFERENCES users(user_id),
-    CONSTRAINT FK_images FOREIGN KEY (band_hero_image_id) REFERENCES images(image_id)
+    CONSTRAINT FK_band_manager FOREIGN KEY (band_manager_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE images (
@@ -40,7 +40,14 @@ CREATE TABLE images (
     band_id int NOT NULL,
     image_link VARCHAR(200) NOT NULL,
     CONSTRAINT PK_image PRIMARY KEY (image_id),
-    CONSTRAINT FK_bands FOREIGN KEY (band_id) REFERENCES bands(band_id)
+    CONSTRAINT FK_image_band FOREIGN KEY (band_id) REFERENCES bands(band_id)
+);
+
+CREATE TABLE user_band (
+    user_id int NOT NULL,
+    band_id int NOT NULL,
+    CONSTRAINT FK_user_band_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+    CONSTRAINT FK_user_band_band FOREIGN KEY (band_id) REFERENCES bands(band_id)
 );
 
 CREATE TABLE genres (
@@ -52,8 +59,8 @@ CREATE TABLE genres (
 CREATE TABLE band_genre (
     band_id int NOT NULL,
     genre_id int NOT NULL,
-    CONSTRAINT FK_bands FOREIGN KEY (band_id) REFERENCES bands(band_id),
-    CONSTRAINT FK_genres FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+    CONSTRAINT FK_band_genre_band FOREIGN KEY (band_id) REFERENCES bands(band_id),
+    CONSTRAINT FK_band_genre_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
 );
 
 CREATE TABLE messages (
@@ -61,7 +68,7 @@ CREATE TABLE messages (
     message_content varchar(200) NOT NULL,
     message_time timestamp NOT NULL,
     message_sender int NOT NULL,
-    CONSTRAINT FK_bands FOREIGN KEY (message_sender) REFERENCES bands(band_id)
+    CONSTRAINT FK_message_sender FOREIGN KEY (message_sender) REFERENCES bands(band_id)
 );
 
 CREATE TABLE events (
@@ -70,7 +77,7 @@ CREATE TABLE events (
     event_time timestamp NOT NULL,
     event_address varchar(100) NOT NULL,
     event_host int NOT NULL,
-    CONSTRAINT FK_bands FOREIGN KEY (event_host) REFERENCES bands(band_id)
+    CONSTRAINT FK_event_host FOREIGN KEY (event_host) REFERENCES bands(band_id)
 );
 
 COMMIT TRANSACTION;
