2ec91cc853e6291fbaebdbae2161548b2cfb68fd
Taylor Quas <tquas24@gmail.com>
2024-10-01 10:08:27 -0400

    m

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <Header>
+      <h1>Welcome, {{ username }}</h1>
+      <section>
+        <button @cick="search" id="search" placeholder="Search">Search</button>
+        <input type="search" id="search" placeholder="Search bands..." />
+        <button type="button" id="logout" value="Logout" @click="logout">Logout</button>
+        <p>You must be authenticated to see this</p>
+      </section>
+    </Header>
   </div>
 </template>
 
 <script>
 export default {
+  components: {
+
+  },
+  data() {
+    return {
+      username: '',
+      isFocused: false,
+    };
+  },
+  methods: {
+    logout() {
+      this.$store.commit('LOGOUT');
+      this.$router.push('/login');
+    },
+  },
 };
 </script>
