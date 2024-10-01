5ebfb5c630f9e9a191099c8ba84d847a6f4e91eb
Taylor Quas <tquas24@gmail.com>
2024-10-01 10:21:26 -0400

    m

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -4,11 +4,26 @@
       <h1>Welcome, {{ username }}</h1>
       <section>
         <button @cick="search" id="search" placeholder="Search">Search</button>
-        <input type="search" id="search" placeholder="Search bands..." />
-        <button type="button" id="logout" value="Logout" @click="logout">Logout</button>
+        <input type="search" id="search" placeholder="Search bands..." @focus="isFocused=true" @blur="isFocused=false" />
         <p>You must be authenticated to see this</p>
       </section>
     </Header>
+
+    <div class="main-content">
+      <aside v-if="showInbox" class="inbox">
+        <h2>Inbox</h2>
+        <ul>
+          <li v-for="message in inboxMessages" :key="message.id">{{ message.content }}</li>
+        </ul>
+      </aside>
+      
+      <div class="bands-list">
+        <h2>Your Bands</h2>
+        <ul>
+          <li v-for="band in bands" :key="band.id">{{ band.name }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,10 +39,16 @@ export default {
     };
   },
   methods: {
-    logout() {
-      this.$store.commit('LOGOUT');
-      this.$router.push('/login');
-    },
+
   },
+  computed: {
+    inboxCount() {
+      return this.inboxMessages.length;
+    },
+  }
 };
 </script>
+
+<style>
+
+</style>
