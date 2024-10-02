de5715d831e0ed925d66e07a072ab93d99160511
Taylor Quas <tquas24@gmail.com>
2024-10-02 13:25:42 -0400

    message filtering by exp date added - untested

--- vue/src/components/Inbox.vue
+++ vue/src/components/Inbox.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>This is the inbox</h2>
-    <div v-for="message in messages" :key=message.messageId>
+    <div v-for="message in filteredMessages" :key=message.messageId>
     <h3>Message: {{ message.message.messageContent }}</h3>
     <h4>From: {{ message.bandName }}</h4>
     </div>
@@ -17,6 +17,15 @@ export default {
         messages: [],
         MessageService,
       };
+    },
+    computed: {
+      filteredMessages() {
+        const currentTime = new Date();
+        return this.messages.filter(message => {
+          const expirationTime = new Date(message.messageTimeExpiration);
+          return expirationTime > currentTime;
+        });
+      }
     },
     created() {
       MessageService.getUserInbox().then(response => {
