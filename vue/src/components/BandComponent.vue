874ed51b34a85bcff52b7bda952ae17c9a7be8b4
Ian ODonnell <ianodonnellk@gmail.com>
2024-10-02 13:48:56 -0400

    Band View

--- vue/src/components/BandComponent.vue
+++ vue/src/components/BandComponent.vue
@@ -48,7 +48,7 @@ export default {
                     this.selectedGenres.some(genre => band.genres.includes(genre));
                 return matchesSearch && matchesGenre;
             });
-        }
+        },
     },
     watch: {
         searchQuery() {
