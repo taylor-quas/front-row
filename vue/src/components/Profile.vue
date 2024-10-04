b1487d009f1e88fffda8c35065de4739135e7602
Ian ODonnell <ianodonnellk@gmail.com>
2024-10-04 13:25:54 -0400

    Followed count on profile

--- vue/src/components/Profile.vue
+++ vue/src/components/Profile.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="profile">
     <h2>This is the profile</h2>
+    <h2>You are following: {{ followedBands.length }} bands</h2>
   </div>
 </template>
 
 <script>
-export default {
+import BandService from '../services/BandService'
 
+export default {
+  data(){
+    return{
+      followedBands: [],
+    }
+  },
+  created() {
+    this.fetchFollowedBands()
+  },
+  methods: {
+    fetchFollowedBands() {
+        BandService.getFollowedBands().then(response => {
+            this.followedBands = response.data;
+            console.log(this.followedBands);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+      },
+  }
 }
 </script>
 
