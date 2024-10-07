b86bd0deda568702072a26280bd3dbfdf9c9f981
Seth Neal <sethaneal1@gmail.com>
2024-10-07 12:47:33 -0400

    updated manager view

--- vue/src/views/ManagerView.vue
+++ vue/src/views/ManagerView.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="manager-view">
     <outbox id="outbox"></outbox>
-    <div id="managed-band">
+    <div id="managed-bands">
       <BandComponent v-for="band in managedBands" :key="band.bandId" :band="band" />
     </div>
     </div>
@@ -11,23 +11,54 @@
 <script>
 import Outbox from '../components/Outbox.vue';
 import BandComponent from '../components/BandComponent.vue';
+import BandService from '../services/BandService';
 
 export default {
   components: {
       Outbox,
+      BandComponent
   },
   data() {
     return {
-      showCreateBand: false
+      showCreateBand: false,
+      managedBands: [],
     }
+  },
+  created() {
+    this.fetchManagedBands();
+  },
+  methods: {
+    fetchManagedBands() {
+      BandService.getBands().then(response => {
+        this.managedBands = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    },
   }
 }
 </script>
 
 <style scoped>
 #manager-view {
-  display: flex;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-areas: 
+    "outbox managed-bands";
   padding: 20px;
+}
+
+#managed-bands {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  width: 100%;
+  grid-area: managed-bands;
+}
+
+#outbox {
+  grid-area: outbox;
 }
 
 </style>
