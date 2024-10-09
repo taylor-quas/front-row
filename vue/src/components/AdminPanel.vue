76b0c1e8fa2255e3b662aa6e093a56b41e5535a1
Ian ODonnell <ianodonnellk@gmail.com>
2024-10-09 11:10:03 -0400

    Admin panel and profile

--- vue/src/components/AdminPanel.vue
+++ vue/src/components/AdminPanel.vue
@@ -2,26 +2,30 @@
   <div id="admin">
     <h2>Admin Panel</h2>
 
-    <div class="genre-list">    
-        {{ genres }}
-        {{genreToAdd}}
-        <form @submit.prevent="updateGenres">
-            <label v-for="genre in genres" :key="genre">
-              <br />
-              <input
-                type="checkbox" 
-                v-model="genre.visible"
-              /> <p style="text-transform: capitalize; color: black;">{{ genre.genreName }}</p>
-            </label>
-            <button type="submit" @click="updateGenres">Update Genres</button>
-        </form>
-      </div>
-      <div>
-        <form @submit.prevent="">
-            <input type="text" v-model="genreToAdd.name">
-            <button type="submit" @click="addGenre">Add Genre</button>
-        </form>
-      </div>
+    <div style="display: flex; justify-content: center; margin: 5vw; gap: 10vw;">
+        <div>
+            <h3>Update Usable Genres</h3>
+            <form @submit.prevent="updateGenres">
+                <div class="genre-list">
+                    <label class="genre-item" v-for="genre in genres" :key="genre">
+                      <br/>
+                      <input
+                        type="checkbox"
+                        v-model="genre.visible"
+                      /> <p style="text-transform: capitalize; color: white;">{{ genre.genreName }}</p>
+                    </label>
+                </div>
+                <button type="submit" @click="updateGenres">Update Genres</button>
+            </form>
+        </div>
+        <div id="add-genre">
+            <h3>Add New Genre</h3>
+            <form @submit.prevent="">
+                <input type="text" v-model="genreToAdd.name">
+                <button type="submit" @click="addGenre">Add Genre</button>
+            </form>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -69,5 +73,28 @@ export default {
     #admin {
         margin-block: 12vh;
     }
+
+
+    .genre-item {
+        display: flex;
+        padding: 0.5em;
+        align-items: center;
+
+    }
+
+    input {
+        margin: 4px;
+    }
+
+    button {
+  margin-top: 1em;
+  color: black;
+  border-radius: 20px;
+  justify-content: center;
+  width: fit-content;
+  min-width: 120px;
+  height: fit-content;
+  min-height: 30px;
+}
 
 </style>
